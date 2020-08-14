f0b58b19eaf9a64b52e04cb585b2cfd3964846b2
Anton Kalinevich <klnvch@sam-solutions.net>
2020-08-14 16:21:58 +0300

    logs

--- GreenHouseController/app/src/main/java/com/klnvch/greenhousecontroller/LogsActivity.java
+++ GreenHouseController/app/src/main/java/com/klnvch/greenhousecontroller/LogsActivity.java
@@ -2,17 +2,42 @@ package com.klnvch.greenhousecontroller;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
 
 import com.klnvch.greenhousecontroller.databinding.ActivityLogsBinding;
 
 public class LogsActivity extends AppCompatActivity {
-    private ActivityLogsBinding binding;
+    private static final int NUM_PAGES = 2;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_logs);
+        ActivityLogsBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_logs);
+        PagerAdapter pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        binding.viewPager.setAdapter(pagerAdapter);
+    }
+
+    private static class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return LogsFragment.newInstance(position);
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
     }
 }
